9e4043c38bb3aade2ef2a5941630fe723b38803a
programming-he-ro <83583681+programming-he-ro@users.noreply.github.com>
2023-06-20 15:42:39 +0000

    apply button tk

--- app.py
+++ app.py
@@ -2,10 +2,38 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id':1,
+    'title': 'Data Analyst',
+    'location':'Delhi, India',
+    'salary':'Rs. 10,00,000'
+  },
+  {
+    'id':2,
+    'title': 'Data Scientist',
+    'location':'Bengaluru, India',
+    'salary':'Rs. 15,00,000'
+  },
+  {
+    'id':3,
+    'title': 'Frontend Engineer',
+    'location':'Remote',
+    
+  },
+  {
+    'id':4,
+    'title': 'Backend Engineer',
+    'location':'San Francisco',
+    'salary':'Rs. 120,000'
+  }
+]
+
+
 
 @app.route("/")
 def helloWorld():
-    return render_template('home.html')
+    return render_template('home.html',jobs=JOBS, company_name="Jovian")
 
 
 if __name__ == "__main__":
